6f91154ab632aba4a68c4e1fdc3731f30db1025b
azeez2002 <al.azeez.nash@gmail.com>
2022-11-11 18:56:53 +0300

    asdsda

---
+++ ShortCourseReg/ShortCourseReg/default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ShortCourseReg._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Login ID="Login1" runat="server" BackColor="#E3EAEB" BorderColor="#E6E2D8" BorderPadding="4" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#333333" Height="187px" TextLayout="TextOnTop" Width="372px" OnAuthenticate="Login1_Authenticate">
+                <InstructionTextStyle Font-Italic="True" ForeColor="Black" Font-Size="2em"/>
+                <LoginButtonStyle BackColor="White" BorderColor="#C5BBAF" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="1.5em" ForeColor="#1C5E55" />
+                <TextBoxStyle Font-Size="0.8em" Width="200px"/>
+                <TitleTextStyle BackColor="#1C5E55" Font-Bold="True" Font-Size="2em" ForeColor="White" />
+            </asp:Login>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ShortCourseReg/ShortCourseReg/default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ShortCourseReg
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["DB"].ConnectionString;
+            string Table_name = null;
+            string user_name = null;
+            string user_Pass = null;
+            if (Login1.UserName.IndexOf("A") != -1)
+            {
+                Table_name = "Admin";
+                user_name = "AdminID";
+                user_Pass = "AdminPassword";
+            }else if(Login1.UserName.IndexOf("I") != -1)
+            {
+                Table_name = "Instructors";
+                user_name = "InstructorsID";
+                user_Pass = "InstructorsPassword";
+            }else if(Login1.UserName.IndexOf("S") != -1)
+            {
+                Table_name = "Students";
+                user_name = "NationalID";
+                user_Pass = "Password";
+            }
+
+
+            string SQL = "SELECT * FROM " + Table_name + "WHERE " + user_name + "= @UserName AND " + user_Pass + "= @UserPassword";
+            SqlCommand cmd = new SqlCommand(SQL, con);
+            SqlParameter p1 = new SqlParameter("UserName", Login1.UserName);
+            SqlParameter p2 = new SqlParameter("UserPassword", Login1.Password);
+            cmd.Parameters.Add(p1);
+            cmd.Parameters.Add(p2);
+            con.Open();
+            SqlDataReader DR = cmd.ExecuteReader();
+            if (DR.HasRows)
+            {
+                DR.Read();
+                FormsAuthentication.RedirectFromLoginPage(Login1.UserName, true);
+                Response.Redirect("Admin/Admin.aspx");
+            }
+            else
+            {
+                Login1.FailureText = "Invalid username or password.";
+            }
+            con.Close();
+        }
+    }
+}
