891ee318b0aee04adf9c9a0f28f20572679612bd
azeez2002 <al.azeez.nash@gmail.com>
2022-11-22 12:09:01 +0300

    11/22/2022

---
+++ ShortCourseReg/ShortCourseReg/AddImahe.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddImahe.aspx.cs" Inherits="ShortCourseReg.AddImahe" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    </head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <table>
+                            <tr>
+                                <td>
+                                    <asp:TextBox ID="tb2" runat="server"></asp:TextBox>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    alkdhakusdgh
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                    <td>
+                        <asp:Image ID="Image1" runat="server" Height="263px" Width="270px" />  
+                <br />
+                <asp:FileUpload ID="FileUpload1" runat="server" />
+                &nbsp;<br />
+                <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Show image" />
+                <br />
+                <asp:Button ID="Butt" runat="server" Text="Save" Width="268px" OnClick="B_Click" />
+
+                <br />
+                <asp:Label ID="saved" runat="server"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ShortCourseReg/ShortCourseReg/AddImahe.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Windows.Forms;
+using FreeTextBoxControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+//using static System.Net.WebRequestMethods;
+
+
+namespace ShortCourseReg
+{
+    public partial class AddImahe : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        protected void B_Click(object sender, EventArgs e)
+        {
+            HttpPostedFile posted_File = FileUpload1.PostedFile;
+            string File_Name = Path.GetFileName(posted_File.FileName);
+            string File_Extension = Path.GetExtension(File_Name);
+
+            if (File_Extension.ToLower() == ".jpg" || File_Extension.ToLower() == ".gif" || File_Extension.ToLower() == ".png" || File_Extension.ToLower() == ".bmp")
+            {
+                Stream stream = posted_File.InputStream;
+                BinaryReader br = new BinaryReader(stream);
+                byte[] Image = br.ReadBytes((int)stream.Length);
+
+
+
+            }
+        }
+
+        
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpPostedFile posted_File = FileUpload1.PostedFile;
+            string File_Name = Path.GetFileName(posted_File.FileName);
+            string File_path = Path.GetFullPath(posted_File.FileName);
+            string File_Extension = Path.GetExtension(File_Name);
+
+            if (File_Extension.ToLower() == ".jpg" || File_Extension.ToLower() == ".gif" || File_Extension.ToLower() == ".png" || File_Extension.ToLower() == ".bmp")
+            {
+                saved.Text = File_path;
+                Image1.ImageUrl = File_path;
+            }
+        }
+    }
+}
